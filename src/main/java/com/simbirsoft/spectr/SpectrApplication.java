89e8b0e17de2ab4c7a3da0c691c9022f21d2ef88
Dmitry <dm.baevsky@gmail.com>
2021-05-28 00:08:42 +0300

    Init Spring Boot

---
+++ src/main/java/com/simbirsoft/spectr/SpectrApplication.java
@@ -0,0 +1,12 @@
+package com.simbirsoft.spectr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpectrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpectrApplication.class, args);
+	}
+}
