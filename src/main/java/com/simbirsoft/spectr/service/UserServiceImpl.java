52f22b52b067f833d20a5dfe68a014da493ebf74
Dmitry <dm.baevsky@gmail.com>
2021-05-29 16:31:19 +0300

    Reconfigure Hibernate Transactional. Add User REST controller.

--- src/main/java/com/simbirsoft/spectr/service/UserServiceImpl.java
+++ src/main/java/com/simbirsoft/spectr/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.simbirsoft.spectr.dao.UserDAO;
 import com.simbirsoft.spectr.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class UserServiceImpl implements UserService {
     private UserDAO userDAO;
 
     @Override
+    @Transactional
     public List<User> getAllUsers() {
         return userDAO.getAllUsers();
     }
