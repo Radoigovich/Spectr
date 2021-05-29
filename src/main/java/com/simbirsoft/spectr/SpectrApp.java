6f2772ed5b32ef4690b610d535876a52384be577
Dmitry <dm.baevsky@gmail.com>
2021-05-29 10:32:11 +0300

    Cleanup code

--- src/main/java/com/simbirsoft/spectr/SpectrApp.java
+++ src/main/java/com/simbirsoft/spectr/SpectrApp.java
@@ -1,26 +1,7 @@
 package com.simbirsoft.spectr;
 
-import com.simbirsoft.spectr.entity.*;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
 public class SpectrApp {
     public static void main(String[] args) {
-        try (SessionFactory sessionFactory = new Configuration()
-                .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(User.class)
-                .addAnnotatedClass(Message.class)
-                .addAnnotatedClass(Room.class)
-                .addAnnotatedClass(Participant.class)
-                .addAnnotatedClass(Role.class)
-                .buildSessionFactory()) {
-            Session session = sessionFactory.getCurrentSession();
 
-            session.beginTransaction();
-            User user = session.get(User.class, 2);
-            session.getTransaction().commit();
-            System.out.println(user);
-        }
     }
 }
